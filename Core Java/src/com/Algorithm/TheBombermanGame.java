e347add560ac1eb73116475dc3ab5505f2ec7605
Dipak Das <dasdipak99@gmail.com>
2019-01-24 15:50:48 +0530

    Commit for ambiguity

--- Core Java/src/com/Algorithm/TheBombermanGame.java
+++ Core Java/src/com/Algorithm/TheBombermanGame.java
@@ -8,14 +8,6 @@ import java.util.Scanner;
  */
 public class TheBombermanGame {
 	static String[] bomberMan(int n, String[] grid) {
-		for(int i=0;i<grid.length;i++){
-			char[] tmp1 = grid[i].toCharArray();
-			for(int j=0;j<grid[0].length();j++){
-				if(tmp1[j] == 'O'){
-					tmp1[j] = '1';
-				}
-				else if(tmp1[j] == '.'){
-					tmp1[j] = 'O';
 		if(n==1){
             return grid;
         }
@@ -43,45 +35,6 @@ public class TheBombermanGame {
 					grid[i]=ala;
 				}
 			}
-			String ala = new String(tmp1);
-			grid[i]=ala;
-			
-		}
-		System.out.println("*******************************");
-		for (int k = 0; k < grid.length; k++){
-			System.out.println(grid[k]);
-		}
-		System.out.println("*******************************");
-		for (int i = 0; i < grid.length; i++){
-			for (int j = 0 ; j < grid[0].length(); j++){
-				if (grid[i].charAt(j) == '1'){
-					if (i-1 >= 0){
-						char[] tmp2 = grid[i-1].toCharArray();
-						tmp2[j] = '.';
-						String ala1 = new String(tmp2);
-						grid[i-1] = ala1;
-					}
-					if (i+1 < grid.length){
-						char[] tmp2 = grid[i+1].toCharArray();
-						/*if (tmp2[j] != '1')*/
-							tmp2[j] = '.';
-						String ala1 = new String(tmp2);
-						grid[i+1] = ala1;
-					}
-					if (j-1 >= 0){
-						char[] tmp2 = grid[i].toCharArray();
-						/*if (tmp2[j-1] != '1')*/
-							tmp2[j-1] = '.';
-						String ala1 = new String(tmp2);
-						grid[i] = ala1;
-					}
-					if (j+1 < grid[0].length()){
-						char[] tmp2 = grid[i].toCharArray();
-						/*if (tmp2[j+1] != '1')*/
-							tmp2[j+1] = '.';
-						tmp2[j] = '.';
-						String ala1 = new String(tmp2);
-						grid[i] = ala1;
 			/*System.out.println("*******************************");
 			for (int k = 0; k < grid.length; k++){
 				System.out.println(grid[k]);
